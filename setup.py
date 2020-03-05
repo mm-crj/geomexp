e04b7c72fcad9ab2167e773bb0fb1a4a9f2a97cb
Mainak Mandal <zenith@gmail.com>
2020-03-05 21:00:47 +0100

    setup details added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='geom_illusion_2d',
+      version='0.1',
+      description='2-d gemometric illusion experiments',
+      url='https://github.com/mm-crj/geom_illusion_2d',
+      author='Mainak Mandal',
+      author_email='mm-crjx@gmail.com',
+      license='GNU LGPLv3',
+      packages=['geom_illusion_2d'],
+      zip_safe=False)
+
