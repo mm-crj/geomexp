c056472aad8a4e08d29b403b7af9675a5f74ef5d
Mainak Mandal <mm.crjx@gmail.com>
2020-03-10 21:58:48 +0100

    rename package, major commit

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='geom_illusion_2d',
+setup(name='geomexp',
       version='0.1',
       description='2-d gemometric illusion experiments',
       url='https://github.com/mm-crj/geom_illusion_2d',
       author='Mainak Mandal',
       author_email='mm.crjx@gmail.com',
       license='GNU LGPLv3',
-      packages=['geom_illusion_2d'],
-      zip_safe=False)
-
+      packages=['geomexp']
+      )
