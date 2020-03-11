a68c5ee1a9d5f9ca00ea1e7efb8a7ba70a612e17
Mainak Mandal <mm.crjx@gmail.com>
2020-03-11 14:08:55 +0100

    Url changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='geomexp',
       version='0.1',
       description='2-d gemometric illusion experiments',
-      url='https://github.com/mm-crj/geom_illusion_2d',
+      url='https://github.com/mm-crj/geomexp',
       author='Mainak Mandal',
       author_email='mm.crjx@gmail.com',
       license='GNU LGPLv3',
